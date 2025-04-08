b3eae2cfd12bdf0fea00c39fe0016f7e95d74cfc
Mostafa Samir <mostafasamir.3338@gmail.com>
2025-04-08 10:03:02 +0200

    quran tab background done 2

--- lib/tabs/quran_tab.dart
+++ lib/tabs/quran_tab.dart
@@ -1,17 +1,25 @@
 import 'package:flutter/material.dart';
-import 'package:islami/common/app_colors.dart';
+import 'package:islami/common/app_assets.dart';
+import 'package:islami/widgets/background_gradient_and_tab_details.dart';
 
 class QuranTab extends StatelessWidget {
   const QuranTab({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return Center(
-      child: Text(
-        'Quran Tap',
-        style: TextStyle(
-            color: AppColors.white, fontSize: 30, fontWeight: FontWeight.w400),
-      ),
-    );
+    double height = MediaQuery.of(context).size.height;
+    double width = MediaQuery.of(context).size.width;
+    return BackgroundGradientAndTabDetails(
+        backgroundImage: AppImages.homeTabBackground,
+        child: Column(
+          children: [
+            Container(
+              height: 20,
+              width: 20,
+              color: Colors.red,
+            )
+          ],
+        ),
+        distanceBetweenIslamiLogoAndTabDetails: height * 0.162 + 20);
   }
 }
